edd790fb0d6c7c0524c8048d4d79a6f0c327233a
Indra2108 <fizipratama21@gmail.com>
2022-02-11 20:43:26 +0700

    Add index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+// const router = require
+const app = express();
+const port = process.env.port || 8080;
+
+const bodyParser = require('express');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/resthub');
+const db = mongoose.connection;
+
+// app.use(router);
+
+app.listen(port, () => {
+    console.log(`Server running at port: ${port}`);
+})
