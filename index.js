8ea220358997f4f105dd66e0d226938cae7b4951
Indra2108 <fizipratama21@gmail.com>
2022-02-11 21:35:20 +0700

    router: Import router at index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// const router = require
+const router = require('./src/router/router');
 const app = express();
 const port = process.env.port || 8080;
 
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 mongoose.connect('mongodb://localhost/resthub');
 const db = mongoose.connection;
 
-// app.use(router);
+app.use(router);
 
 app.listen(port, () => {
     console.log(`Server running at port: ${port}`);
